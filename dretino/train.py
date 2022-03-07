101d7bd085f85c9bfb8bb3112ee43ad19c4c880e
Dineswar <dineshwarreddy619@gmail.com>
2022-03-07 08:04:09 +0530

    added sweep notebook

--- dretino/train.py
+++ dretino/train.py
@@ -3,6 +3,8 @@ import dotenv
 import wandb
 import albumentations as A
 import pandas as pd
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_confusion_matrix
 from albumentations.pytorch import ToTensorV2
 from dretino.dataloader.build_features import DRDataModule
 from dretino.models.predict_model import test
@@ -10,6 +12,45 @@ from dretino.models.train_model import Model, train
 from dretino.models.get_preds import create_preds_data_loader, get_perds
 from dretino.visualization.visualize import show_images, cal_mean, plot_metrics
 from sklearn.model_selection import train_test_split
+
+
+def helper(wab, fast_dev_run, overfit_batches, num_classes=5, **args):
+    file_name, trainer = train(Model, dm,
+                               wab=wab,
+                               fast_dev_run=fast_dev_run,
+                               overfit_batches=overfit_batches,
+                               **args)
+
+    print(file_name)
+
+    if not fast_dev_run:
+        plot_metrics(file_name)
+
+        test(Model, dm,
+             file_name,
+             trainer,
+             wab=wab,
+             fast_dev_run=fast_dev_run,
+             overfit_batches=overfit_batches)
+
+        train_dataloader, val_dataloader, test_dataloader = create_preds_data_loader(df_train, df_valid, df_test,
+                                                                                     train_path=IMG_PATH + 'images_resized',
+                                                                                     valid_path=IMG_PATH + 'images_resized',
+                                                                                     test_path=IMG_PATH + 'test_images_resized',
+                                                                                     transforms=test_transforms)
+
+        preds, y, cfn_mtx = get_perds(file_name, Model, test_dataloader,
+                                      args['loss'], num_classes=5)
+
+        #         wandb.sklearn.plot_confusion_matrix(y, preds, labels=['0','1','2','3','4'])
+
+        # plot_confusion_matrix(conf_mat=cfn_mtx,
+        # class_names=['0', '1', '2', '3', '4'])
+
+        # plt.show()
+
+        return preds, y, cfn_mtx
+
 
 if __name__ == "__main__":
     project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
@@ -82,47 +123,64 @@ if __name__ == "__main__":
                       num_workers=4,
                       batch_size=4)
 
-    args = dict(
-        model_name='resnet50d',
-        num_neurons=512,
-        num_layers=2,
-        dropout=0.2,
-        lr=3e-4,
-        loss='mse',
-        epochs=2,
-        gpus=0,
-        project='DRD',
-        additional_layers=False,
-        save_dir='reports'
-    )
-
-    wab = False
+    wab = True
     fast_dev_run = False
     overfit_batches = False
 
     if wab:
         wandb.login(key=os.getenv('WANDB'))
 
-    file_name, trainer = train(Model, dm,
-                               wab=wab,
-                               fast_dev_run=fast_dev_run,
-                               overfit_batches=overfit_batches,
-                               **args)
-    if not fast_dev_run:
-        plot_metrics(file_name)
+    sweep_config = {
+        "name": "HyperParameterSearch",
+        "method": "random",  # Random search
+        "metric": {  # We want to maximize val_accuracy
+            "name": "test_accuracy",
+            "goal": "maximize"
+        },
+        "parameters": {
+            "num_neurons": {
+                # Choose from pre-defined values
+                "values": [512, 1024, 2048, 4096, 8192]
+            },
+            "num_layers": {
+                # Choose from pre-defined values
+                "values": [2, 3, 4, 5, 6]
+            },
+            "dropout": {
+                "values": [0.2, 0.3, 0.4, 0.5]
+            },
+            "lr": {
+                # log uniform distribution between exp(min) and exp(max)
+                "distribution": "log_uniform",
+                "min": -9.21,  # exp(-9.21) = 1e-4
+                "max": -6.90  # exp(-6.90) = 1e-3
+            },
+            "loss": {
+                "values": ['ce', 'corn']
+            }
+        }
+    }
 
-        test(Model, dm,
-             file_name,
-             trainer,
-             wab=wab,
-             fast_dev_run=fast_dev_run,
-             overfit_batches=overfit_batches)
+    sweep_id = wandb.sweep(sweep_config, project="DRD")
 
-        train_dataloader, val_dataloader, test_dataloader = create_preds_data_loader(df_train, df_valid, df_test,
-                                                                                     train_path=PATH + 'images_resized',
-                                                                                     valid_path=PATH + 'images_resized',
-                                                                                     test_path=PATH + 'test_images_resized',
-                                                                                     transforms=test_transforms)
-        preds, y = get_perds(file_name, Model, val_dataloader,
-                             args['loss'], num_classes=5)
-        print(preds)
+
+    def sweep_iteration():
+        # set up W&B logger
+        wandb.init()  # required to have access to `wandb.config`
+        args = dict(
+            model_name='resnet50d',
+            num_neurons=wandb.config.num_neurons,
+            num_layers=wandb.config.num_layers,
+            dropout=wandb.config.dropout,
+            lr=wandb.config.lr,
+            loss=wandb.config.loss,
+            epochs=1,
+            gpus=0,
+            project='DRD',
+            additional_layers=True,
+            save_dir='reports'
+        )
+
+        preds, y, cfn = helper(wab, fast_dev_run, overfit_batches, num_classes=5, **args)
+
+    wandb.agent(sweep_id,function=sweep_iteration,count=3)
